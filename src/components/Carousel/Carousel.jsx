068ea10e603da03cbd662ac57b3bfae9a08276f0
Valentin Lamothe coulomme <valentinlamothecoulomme@MacBook-Pro-de-Valentin.local>
2021-08-23 20:46:40 -0300

    Proyecto finalizado

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import { img_300, noPicture } from '../../config/config';
+import './Carousel.css'
+
+const handleDragStart = (e) => e.preventDefault();
+
+const Carousel = ({media_type, id}) => {
+
+    const [credits, setCredits] = useState([]);
+
+    const items = credits.map((c) => (
+        <div className="carouselItem">
+          <img
+            src={c.profile_path ? `${img_300}/${c.profile_path}` : noPicture}
+            alt={c?.name}
+            onDragStart={handleDragStart}
+            className="carouselItem__img"
+          />
+          <b className="carouselItem__txt">{c?.name}</b>
+        </div>
+      ));
+    
+      const responsive = {
+        0: {
+          items: 3,
+        },
+        512: {
+          items: 5,
+        },
+        1024: {
+          items: 7,
+        },
+      };
+
+    const FetchCredits = async () => {
+        const { data } = await axios.get(`https://api.themoviedb.org/3/${media_type}/${id}/credits?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
+
+        setCredits(data.cast)
+    };
+
+    useEffect(() => {
+        FetchCredits()
+    }, []);
+
+  return (
+    <AliceCarousel autoPlay infinite disableDotsControls disableButtonsControls responsive={responsive} mouseTracking items={items} />
+  );
+}
+
+export default Carousel;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import './Header.css'
+
+function Header() {
+    return <span onClick={() => window.scroll(0, 0)} className='header'>🎬 Entertainment Hub 🎥</span>
+}
+
+export default Header;
